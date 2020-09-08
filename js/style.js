faf38c0497b9daae42b54c7739ecfe5ee2d4d416
jungseri <jjest2222@naver.com>
2020-09-08 14:39:07 +0900

    슬라이드..

--- js/style.js
+++ js/style.js
@@ -27,32 +27,43 @@ $(document).ready(function () {
   /*슬라이드*/
   $(".lightgallery").lightGallery();
 
-<<<<<<< HEAD
-  const config = {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    slidesPerColumn: 2,
+    spaceBetween: 30,
 
-    type: 'carousel',
-    gap: 5,
-    center: true,
-    // startAt: 0,
-    perView: 3,
-    // autoplay: 1000,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
     breakpoints: {
-      1000: {
-        perView: 2
+      300: {
+        slidesPerView: 1,
+        slidesPerColumn: 2,
+        spaceBetween: 20,
       },
-      600: {
-        perView: 1
-      }
-    }
-  }
+      768: {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        spaceBetween: 20,
+      },
+      1100: {
+        slidesPerView: 3,
+        slidesPerColumn: 2,
+        spaceBetween: 20,
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    // loopedSlides: 5,
+    loopFillGroupWithBlank: true,
 
+  });
 
-
-  new Glide('.slide1', config).mount();
-
-  new Glide('.slide2', config).mount();
-=======
->>>>>>> 252e6f3b6ad202f3d4f6e6fa89b9e557495061e0
   /*탭버튼*/
   var tabAnchor = $(".tabs-nav li a"),
     tabpanel = $(".glide__slides");
@@ -117,83 +128,83 @@ $(document).ready(function () {
 // window.addEventListener("load", function () {
 //   new Glider(document.querySelector(".slide2", config));
 // });
-window.addEventListener("load", function () {
-  new Glider(document.querySelector(".slide1"), {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // dots: '.carousel__indicadores',
-    arrows: {
-      prev: ".carousel__anterior",
-      next: ".carousel__siguiente",
-    },
+// window.addEventListener("load", function () {
+//   new Glider(document.querySelector(".slide1"), {
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     // dots: '.carousel__indicadores',
+//     arrows: {
+//       prev: ".carousel__anterior",
+//       next: ".carousel__siguiente",
+//     },
 
-    responsive: [
-      {
-        // screens greater than >= 775px
-        breakpoint: 300,
-        settings: {
-          // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        // screens greater than >= 775px
-        breakpoint: 550,
-        settings: {
-          // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        // screens greater than >= 1024px
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-    ],
-  });
-});
-window.addEventListener("load", function () {
-  new Glider(document.querySelector(".slide2"), {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // dots: '.carousel__indicadores',
-    arrows: {
-      prev: ".carousel__anterior",
-      next: ".carousel__siguiente",
-    },
+//     responsive: [
+//       {
+//         // screens greater than >= 775px
+//         breakpoint: 300,
+//         settings: {
+//           // Set to `auto` and provide item width to adjust to viewport
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         // screens greater than >= 775px
+//         breakpoint: 550,
+//         settings: {
+//           // Set to `auto` and provide item width to adjust to viewport
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//         },
+//       },
+//       {
+//         // screens greater than >= 1024px
+//         breakpoint: 800,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 3,
+//         },
+//       },
+//     ],
+//   });
+// });
+// window.addEventListener("load", function () {
+//   new Glider(document.querySelector(".slide2"), {
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     // dots: '.carousel__indicadores',
+//     arrows: {
+//       prev: ".carousel__anterior",
+//       next: ".carousel__siguiente",
+//     },
 
-    responsive: [
-      {
-        // screens greater than >= 775px
-        breakpoint: 300,
-        settings: {
-          // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        // screens greater than >= 775px
-        breakpoint: 550,
-        settings: {
-          // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        // screens greater than >= 1024px
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-    ],
-  });
-});
+//     responsive: [
+//       {
+//         // screens greater than >= 775px
+//         breakpoint: 300,
+//         settings: {
+//           // Set to `auto` and provide item width to adjust to viewport
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         // screens greater than >= 775px
+//         breakpoint: 550,
+//         settings: {
+//           // Set to `auto` and provide item width to adjust to viewport
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//         },
+//       },
+//       {
+//         // screens greater than >= 1024px
+//         breakpoint: 800,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 3,
+//         },
+//       },
+//     ],
+//   });
+// });
